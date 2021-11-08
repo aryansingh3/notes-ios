0187e867a9d589b45ee115c1f8a5a06f7ef17a55
aryansingh3 <74758078+aryansingh3@users.noreply.github.com>
2021-11-08 16:00:47 +0530

    Add files via upload
    its a note app which can be used to take notes via ur voice amd keyboard on ur ios watches

---
+++ Note.swift
@@ -0,0 +1,11 @@
+//
+//
+
+//
+
+import Foundation
+
+struct Note: Identifiable, Codable {
+  let id: UUID
+  let text: String
+}
---
+++ NotesApp.swift
@@ -0,0 +1,16 @@
+//
+//  
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        ContentView()
+      }
+    }
+  }
+}
